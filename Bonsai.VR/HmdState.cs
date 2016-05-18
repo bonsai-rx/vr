20ea511a25ae2457ab5b24fed2c1989312f51180
glopes <glopesdev@users.noreply.github.com>
2016-05-18 18:15:37 +0100

    Refactored device pose abstraction draft to allow for multiple device tracking.

---
+++ Bonsai.VR/HmdState.cs
@@ -0,0 +1,18 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.VR
+{
+    public class HmdState
+    {
+        public Matrix4 HeadPose;
+        public Matrix4 LeftViewMatrix;
+        public Matrix4 LeftProjectionMatrix;
+        public Matrix4 RightViewMatrix;
+        public Matrix4 RightProjectionMatrix;
+    }
+}
