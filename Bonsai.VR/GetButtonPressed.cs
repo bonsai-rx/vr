580cede81201a09f80b912846b5fbd6161d2f2ae
glopes <glopesdev@users.noreply.github.com>
2017-04-24 19:38:41 +0100

    Added GetButtonPressed and GetButtonTouched nodes to retrieve button state.

---
+++ Bonsai.VR/GetButtonPressed.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.VR
+{
+    public class GetButtonPressed : Transform<ControllerState, bool>
+    {
+        public VRButtonId Button { get; set; }
+
+        public override IObservable<bool> Process(IObservable<ControllerState> source)
+        {
+            return source.Select(input => input.GetButtonPressed(Button));
+        }
+    }
+}
