48ecc1b4cc0899bac365e3d7c0fec55acd7fedef
glopes <glopes@localhost>
2017-04-24 19:12:29 +0100

    Refactored GetControllerState to make it easier to readout button states.

--- Bonsai.VR/ControllerState.cs
+++ Bonsai.VR/ControllerState.cs
@@ -1,9 +1,4 @@
 ﻿using OpenTK;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Valve.VR;
 
 namespace Bonsai.VR
@@ -11,17 +6,44 @@ namespace Bonsai.VR
     public class ControllerState
     {
         public Matrix4 ControllerPose;
-        public uint PacketNum;
+        public uint PacketNumber;
         public VRControllerAxis_t Axis0; //For Vive: (x,y) = TouchPad x,y
         public VRControllerAxis_t Axis1; //For Vive: x = Trigger position
         public VRControllerAxis_t Axis2; //For vive not used
         public VRControllerAxis_t Axis3; //For vive not used
         public VRControllerAxis_t Axis4; //For vive not used
-
-        public ulong ButtonPressedId;
-        public ulong ButtonTouchedId;
-        public string ButtonPressedName;
-        public string ButtonTouchedName;
+        public ulong ButtonPressed;
+        public ulong ButtonTouched;
         public bool IsValid;
+
+        public bool GetButtonPressed(VRButtonId button)
+        {
+            return (ButtonPressed & (1ul << (int)button)) != 0;
+        }
+
+        public bool GetButtonTouched(VRButtonId button)
+        {
+            return (ButtonTouched & (1ul << (int)button)) != 0;
+        }
+    }
+
+    public enum VRButtonId
+    {
+        System = EVRButtonId.k_EButton_System,
+        ApplicationMenu = EVRButtonId.k_EButton_ApplicationMenu,
+        Grip = EVRButtonId.k_EButton_Grip,
+        DPadLeft = EVRButtonId.k_EButton_DPad_Left,
+        DPadUp = EVRButtonId.k_EButton_DPad_Up,
+        DPadRight = EVRButtonId.k_EButton_DPad_Right,
+        DPadDown = EVRButtonId.k_EButton_DPad_Down,
+        A = EVRButtonId.k_EButton_A,
+        Axis0 = EVRButtonId.k_EButton_Axis0,
+        Axis1 = EVRButtonId.k_EButton_Axis1,
+        Axis2 = EVRButtonId.k_EButton_Axis2,
+        Axis3 = EVRButtonId.k_EButton_Axis3,
+        Axis4 = EVRButtonId.k_EButton_Axis4,
+        SteamVRTouchpad = EVRButtonId.k_EButton_SteamVR_Touchpad,
+        SteamVRTrigger = EVRButtonId.k_EButton_SteamVR_Trigger,
+        DashboardBack = EVRButtonId.k_EButton_Dashboard_Back
     }
 }
--- Bonsai.VR/GetControllerState.cs
+++ Bonsai.VR/GetControllerState.cs
@@ -1,96 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reactive.Linq;
+using Valve.VR;
 
 namespace Bonsai.VR
 {
-    using OpenTK;
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Reactive.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using Valve.VR;
-
- 
-    enum Buttons
-    {
-        System = (int)EVRButtonId.k_EButton_System, // reserved
-        ApplicationMenu = (int)EVRButtonId.k_EButton_ApplicationMenu,
-        Grip = (int)EVRButtonId.k_EButton_Grip,
-        Axis0 = (int)EVRButtonId.k_EButton_Axis0,
-        Axis1 = (int)EVRButtonId.k_EButton_Axis1,
-        Axis2 = (int)EVRButtonId.k_EButton_Axis2,
-        Axis3 = (int)EVRButtonId.k_EButton_Axis3,
-        Axis4 = (int)EVRButtonId.k_EButton_Axis4,
-        Touchpad = (int)EVRButtonId.k_EButton_SteamVR_Touchpad,
-        Trigger = (int)EVRButtonId.k_EButton_SteamVR_Trigger
-    };
-
     public class GetControllerState : Transform<VRDataFrame, ControllerState>
     {
+        public int? Index { get; set; }
 
-        VRControllerState_t prevState;
-        TrackedDevicePose_t pose;
-        ETrackingUniverseOrigin trackingSpace = ETrackingUniverseOrigin.TrackingUniverseStanding;
-
-           
-
-        bool valid { get; set; }
-
-        public uint Index { get; set; }
-
-        public ETrackedControllerRole ControllerRole { get; set; }
-
-        public GetControllerState()
-        {
-                
-        }
-
+        public ETrackedControllerRole? ControllerRole { get; set; }
 
         public override IObservable<ControllerState> Process(IObservable<VRDataFrame> source)
         {
             return Observable.Defer(() =>
             {
-                    
+                var state = new VRControllerState_t();
                 return source.Select(input =>
                 {
-                    var state = new VRControllerState_t();
-                    var compositor = OpenVR.Compositor;
-                    //compositor.SetTrackingSpace(trackingSpace);
-
-                    prevState = state;
-                    valid = input.Hmd.GetControllerStateWithPose(trackingSpace, Index, ref state, ref pose);
-
-                    ControllerState result_state = new ControllerState();
-
-                    result_state.Axis0 = state.rAxis0;
-                    result_state.Axis1 = state.rAxis1;
-                    result_state.Axis2 = state.rAxis2;
-                    result_state.Axis3 = state.rAxis3;
-                    result_state.Axis4 = state.rAxis4;
-                    result_state.ButtonPressedId = state.ulButtonPressed;
-                    result_state.ButtonTouchedId = state.ulButtonTouched;
-                    result_state.PacketNum = state.unPacketNum;
-                    result_state.ControllerPose = input.RenderPoses[Index].DeviceToAbsolutePose;
-                    result_state.IsValid = input.RenderPoses[Index].IsValid;
-
-                    foreach (string button in Enum.GetNames(typeof(Buttons)))
+                    var index = (uint)Index.GetValueOrDefault();
+                    var role = ControllerRole.GetValueOrDefault();
+                    if (role != ETrackedControllerRole.Invalid)
                     {
-                        if((state.ulButtonPressed & (1ul << (int)Enum.Parse(typeof(Buttons), button))) != 0){
-                            result_state.ButtonPressedName = button;
-                        }
-
-                        if ((state.ulButtonTouched & (1ul << (int)Enum.Parse(typeof(Buttons), button))) != 0)
-                        {
-                            result_state.ButtonTouchedName = button;
-                        }
+                        index = input.Hmd.GetTrackedDeviceIndexForControllerRole(role);
                     }
 
-                    return result_state;
+                    var valid = input.Hmd.GetControllerState(index, ref state);
+                    var result = new ControllerState();
+                    result.Axis0 = state.rAxis0;
+                    result.Axis1 = state.rAxis1;
+                    result.Axis2 = state.rAxis2;
+                    result.Axis3 = state.rAxis3;
+                    result.Axis4 = state.rAxis4;
+                    result.ButtonPressed = state.ulButtonPressed;
+                    result.ButtonTouched = state.ulButtonTouched;
+                    result.PacketNumber = state.unPacketNum;
+                    result.ControllerPose = input.RenderPoses[index].DeviceToAbsolutePose;
+                    result.IsValid = valid && input.RenderPoses[index].IsValid;
+                    return result;
                 });
             });
         }
