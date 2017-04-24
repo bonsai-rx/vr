32ed51740309e7b2f6032db0ea0cb6e3a44e434b
glopes <glopes@localhost>
2017-04-24 19:12:52 +0100

    Removed unused using statements and local variables.

--- Bonsai.VR/SubmitTextures.cs
+++ Bonsai.VR/SubmitTextures.cs
@@ -1,12 +1,8 @@
 ﻿using Bonsai.Shaders;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing.Design;
-using System.Linq;
 using System.Reactive.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Valve.VR;
 
 namespace Bonsai.VR
