2b938d0fd36a6c0b2e96c81fd8faf7ee0fc89cf3
glopes <glopesdev@users.noreply.github.com>
2017-04-26 12:25:32 +0100

    Updated GetControllerState to correctly handle invalid indices.

--- Bonsai.VR/GetControllerState.cs
+++ Bonsai.VR/GetControllerState.cs
@@ -18,25 +18,28 @@ namespace Bonsai.VR
                 var state = new VRControllerState_t();
                 return source.Select(input =>
                 {
-                    var index = (uint)Index.GetValueOrDefault();
+                    var result = new ControllerState();
+                    var index = Index.GetValueOrDefault();
                     var role = ControllerRole.GetValueOrDefault();
                     if (role != ETrackedControllerRole.Invalid)
                     {
-                        index = input.Hmd.GetTrackedDeviceIndexForControllerRole(role);
+                        index = (int)input.Hmd.GetTrackedDeviceIndexForControllerRole(role);
                     }
 
-                    var valid = input.Hmd.GetControllerState(index, ref state);
-                    var result = new ControllerState();
-                    DataHelper.ToVector2(ref state.rAxis0, out result.Axis0);
-                    DataHelper.ToVector2(ref state.rAxis1, out result.Axis1);
-                    DataHelper.ToVector2(ref state.rAxis2, out result.Axis2);
-                    DataHelper.ToVector2(ref state.rAxis3, out result.Axis3);
-                    DataHelper.ToVector2(ref state.rAxis4, out result.Axis4);
-                    result.ButtonPressed = state.ulButtonPressed;
-                    result.ButtonTouched = state.ulButtonTouched;
-                    result.PacketNumber = state.unPacketNum;
-                    result.ControllerPose = input.RenderPoses[index].DeviceToAbsolutePose;
-                    result.IsValid = valid && input.RenderPoses[index].IsValid;
+                    if (index >= 0)
+                    {
+                        var valid = input.Hmd.GetControllerState((uint)index, ref state);
+                        DataHelper.ToVector2(ref state.rAxis0, out result.Axis0);
+                        DataHelper.ToVector2(ref state.rAxis1, out result.Axis1);
+                        DataHelper.ToVector2(ref state.rAxis2, out result.Axis2);
+                        DataHelper.ToVector2(ref state.rAxis3, out result.Axis3);
+                        DataHelper.ToVector2(ref state.rAxis4, out result.Axis4);
+                        result.ButtonPressed = state.ulButtonPressed;
+                        result.ButtonTouched = state.ulButtonTouched;
+                        result.PacketNumber = state.unPacketNum;
+                        result.ControllerPose = input.RenderPoses[index].DeviceToAbsolutePose;
+                        result.IsValid = valid && input.RenderPoses[index].IsValid;
+                    }
                     return result;
                 });
             });
