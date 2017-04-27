17934dfc4cf8346513a5dd64378c2df0e7389efa
glopes <glopesdev@users.noreply.github.com>
2017-04-27 10:33:41 +0100

    Updated SubmitTextures to work on dynamic texture input.

--- Bonsai.VR/SubmitTextures.cs
+++ Bonsai.VR/SubmitTextures.cs
@@ -7,70 +7,20 @@ using Valve.VR;
 
 namespace Bonsai.VR
 {
-    public class SubmitTextures : Sink
+    public class SubmitTextures : Sink<Tuple<Texture, Texture>>
     {
-        [Editor("Bonsai.Shaders.Configuration.Design.TextureConfigurationEditor, Bonsai.Shaders.Design", typeof(UITypeEditor))]
-        public string LeftEyeTexture { get; set; }
-
-        [Editor("Bonsai.Shaders.Configuration.Design.TextureConfigurationEditor, Bonsai.Shaders.Design", typeof(UITypeEditor))]
-        public string RightEyeTexture { get; set; }
-
-        public override IObservable<TSource> Process<TSource>(IObservable<TSource> source)
+        public override IObservable<Tuple<Texture, Texture>> Process(IObservable<Tuple<Texture, Texture>> source)
         {
-            return Observable.Defer(() =>
+            return source.Do(input =>
             {
-                var leftEyeTexture = 0;
-                var leftName = LeftEyeTexture;
-                if (string.IsNullOrEmpty(leftName))
-                {
-                    throw new InvalidOperationException("A texture sampler name must be specified.");
-                }
-
-                var rightEyeTexture = 0;
-                var rightName = RightEyeTexture;
-                if (string.IsNullOrEmpty(rightName))
-                {
-                    throw new InvalidOperationException("A texture sampler name must be specified.");
-                }
-
-                return source.CombineEither(
-                    ShaderManager.WindowSource.Do(window =>
-                    {
-                        window.Update(() =>
-                        {
-                            var tex = window.Textures[leftName];
-                            if (tex == null)
-                            {
-                                throw new InvalidOperationException(string.Format(
-                                    "The texture \"{0}\" was not found.",
-                                    leftName));
-                            }
-
-                            leftEyeTexture = tex.Id;
-
-                            tex = window.Textures[rightName];
-                            if (tex == null)
-                            {
-                                throw new InvalidOperationException(string.Format(
-                                    "The texture \"{0}\" was not found.",
-                                    leftName));
-                            }
-
-                            rightEyeTexture = tex.Id;
-                        });
-                    }),
-                    (input, window) =>
-                    {
-                        Texture_t leftEye, rightEye;
-                        leftEye.handle = (IntPtr)leftEyeTexture;
-                        rightEye.handle = (IntPtr)rightEyeTexture;
-                        leftEye.eColorSpace = rightEye.eColorSpace = EColorSpace.Auto;
-                        leftEye.eType = rightEye.eType = EGraphicsAPIConvention.API_OpenGL;
-                        OpenVR.Compositor.Submit(EVREye.Eye_Left, ref leftEye, IntPtr.Zero, EVRSubmitFlags.Submit_Default);
-                        OpenVR.Compositor.Submit(EVREye.Eye_Right, ref rightEye, IntPtr.Zero, EVRSubmitFlags.Submit_Default);
-                        OpenVR.Compositor.PostPresentHandoff();
-                        return input;
-                    });
+                Texture_t leftEye, rightEye;
+                leftEye.handle = (IntPtr)input.Item1.Id;
+                rightEye.handle = (IntPtr)input.Item2.Id;
+                leftEye.eColorSpace = rightEye.eColorSpace = EColorSpace.Auto;
+                leftEye.eType = rightEye.eType = EGraphicsAPIConvention.API_OpenGL;
+                OpenVR.Compositor.Submit(EVREye.Eye_Left, ref leftEye, IntPtr.Zero, EVRSubmitFlags.Submit_Default);
+                OpenVR.Compositor.Submit(EVREye.Eye_Right, ref rightEye, IntPtr.Zero, EVRSubmitFlags.Submit_Default);
+                OpenVR.Compositor.PostPresentHandoff();
             });
         }
     }
