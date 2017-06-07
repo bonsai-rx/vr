a66aed034a5b099e342d6c131694a56218b90eae
glopes <glopes@localhost>
2017-06-07 11:06:50 +0100

    Added GetTrackingReference module to retrieve pose of lighthouse basestations.

---
+++ Bonsai.VR/TrackingReference.cs
@@ -0,0 +1,15 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.VR
+{
+    public class TrackingReference
+    {
+        public Matrix4 ReferencePose;
+        public bool IsValid;
+    }
+}
