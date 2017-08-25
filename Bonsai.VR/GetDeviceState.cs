9456483ba0b87fa0b4e015b2bbd64a4ccc78565d
glopes <glopes@localhost>
2017-08-25 11:35:03 +0100

    Added support for custom tracked devices.

---
+++ Bonsai.VR/GetDeviceState.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using Valve.VR;
+
+namespace Bonsai.VR
+{
+    [Description("Extracts the specified tracked device state from the specified VR system data.")]
+    public class GetDeviceState : Transform<VRDataFrame, DeviceState>
+    {
+        [Description("The serial number of the device for which to extract the state.")]
+        public string SerialNumber { get; set; }
+
+        public override IObservable<DeviceState> Process(IObservable<VRDataFrame> source)
+        {
+            return source.Select(input =>
+            {
+                var index = -1;
+                var serial = SerialNumber;
+                var result = new DeviceState();
+                for (uint i = 0; i < input.RenderPoses.Length; i++)
+                {
+                    if (serial == input.Hmd.GetStringTrackedDeviceProperty(i, ETrackedDeviceProperty.Prop_SerialNumber_String))
+                    {
+                        index = (int)i;
+                        break;
+                    }
+                }
+
+                if (index >= 0 && index < input.RenderPoses.Length)
+                {
+                    var deviceIndex = index;
+                    result.Velocity = input.RenderPoses[deviceIndex].Velocity;
+                    result.AngularVelocity = input.RenderPoses[deviceIndex].AngularVelocity;
+                    result.DevicePose = input.RenderPoses[deviceIndex].DeviceToAbsolutePose;
+                    result.IsValid = input.RenderPoses[deviceIndex].IsValid;
+                }
+                return result;
+            });
+        }
+    }
+}
---
+++ Bonsai.VR/VRExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Valve.VR;
+
+namespace Bonsai.VR
+{
+    static class VRExtensions
+    {
+        public static string GetStringTrackedDeviceProperty(this CVRSystem hmd, uint unDeviceIndex, ETrackedDeviceProperty prop)
+        {
+            var error = ETrackedPropertyError.TrackedProp_Success;
+            var builder = new StringBuilder((int)OpenVR.k_unTrackingStringSize);
+            var result = hmd.GetStringTrackedDeviceProperty(unDeviceIndex, prop, builder, (int)OpenVR.k_unTrackingStringSize, ref error);
+            if (error != ETrackedPropertyError.TrackedProp_Success)
+            {
+                throw new VRException(hmd.GetPropErrorNameFromEnum(error));
+            }
+            return builder.ToString();
+        }
+    }
+}
