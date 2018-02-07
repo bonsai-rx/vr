605a27845e7675bebeabe746df8c206070f99c52
glopes <glopesdev@users.noreply.github.com>
2018-02-07 01:11:28 +0000

    Added type converter to allow listing custom device serial numbers.

--- Bonsai.VR/GetDeviceState.cs
+++ Bonsai.VR/GetDeviceState.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Reactive.Linq;
@@ -9,6 +10,7 @@ namespace Bonsai.VR
     [Description("Extracts the specified tracked device state from the specified VR system data.")]
     public class GetDeviceState : Transform<VRDataFrame, DeviceState>
     {
+        [TypeConverter(typeof(SerialNumberConverter))]
         [Description("The serial number of the device for which to extract the state.")]
         public string SerialNumber { get; set; }
 
@@ -39,6 +41,51 @@ namespace Bonsai.VR
                 }
                 return result;
             });
+        }
+
+        class SerialNumberConverter : StringConverter
+        {
+            public override bool GetStandardValuesSupported(ITypeDescriptorContext context)
+            {
+                return true;
+            }
+
+            public override TypeConverter.StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
+            {
+                var initialized = false;
+                var system = OpenVR.System;
+                try
+                {
+                    if (system == null)
+                    {
+                        var initError = EVRInitError.None;
+                        system = OpenVR.Init(ref initError, EVRApplicationType.VRApplication_Other);
+                        if (initError != EVRInitError.None)
+                        {
+                            return base.GetStandardValues(context);
+                        }
+                        else initialized = true;
+                    }
+
+                    var serialNumbers = new List<string>();
+                    var devices = new uint[OpenVR.k_unMaxTrackedDeviceCount];
+                    var count = system.GetSortedTrackedDeviceIndicesOfClass(ETrackedDeviceClass.Other, devices, 0);
+                    for (uint i = 0; i < count; i++)
+                    {
+                        var serial = system.GetStringTrackedDeviceProperty(i, ETrackedDeviceProperty.Prop_SerialNumber_String);
+                        serialNumbers.Add(serial);
+                    }
+
+                    return new StandardValuesCollection(serialNumbers);
+                }
+                finally
+                {
+                    if (initialized)
+                    {
+                        OpenVR.Shutdown();
+                    }
+                }
+            }
         }
     }
 }
