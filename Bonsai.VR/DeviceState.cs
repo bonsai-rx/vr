e94c5f5b2e6523d8b0fa900a6ff8dd68f4cc06a8
glopes <glopesdev@users.noreply.github.com>
2018-02-14 17:03:57 +0000

    Added TriggerHapticPulse operator.

--- Bonsai.VR/DeviceState.cs
+++ Bonsai.VR/DeviceState.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Valve.VR;
 
 namespace Bonsai.VR
 {
@@ -14,5 +15,14 @@ namespace Bonsai.VR
         public Vector3 AngularVelocity;
         public Matrix4 DevicePose;
         public bool IsValid;
+
+        public void TriggerHapticPulse(int axis, int durationMicroseconds)
+        {
+            var system = OpenVR.System;
+            if (system != null)
+            {
+                system.TriggerHapticPulse((uint)DeviceIndex, (uint)axis, (ushort)durationMicroseconds);
+            }
+        }
     }
 }
