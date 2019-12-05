ba64e26a90c1984e149737acf79ee6918414679d
glopes <glopesdev@users.noreply.github.com>
2019-12-05 12:39:41 +0000

    Added support for tracked camera devices.

---
+++ Bonsai.VR/TrackedCameraState.cs
@@ -0,0 +1,22 @@
+﻿using OpenCV.Net;
+using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Valve.VR;
+
+namespace Bonsai.VR
+{
+    public class TrackedCameraState : DeviceState
+    {
+        public uint Width;
+        public uint Height;
+        public uint BytesPerPixel;
+        public uint FrameSequence;
+        public ulong FrameExposureTime;
+        public EVRTrackedCameraFrameType FrameType;
+        internal ulong Handle;
+    }
+}
